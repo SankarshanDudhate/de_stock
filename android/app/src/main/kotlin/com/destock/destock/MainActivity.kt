b649eafc02fa01d302752bfa39ca2b1e997145cf
Hitesh Joshi <joshihitesh090@gmail.com>
2020-09-08 00:42:42 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/destock/destock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.destock.destock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/destock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.destock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
