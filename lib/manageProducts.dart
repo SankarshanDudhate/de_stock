8e40c4ddc60bb8c093b607a3f53cc144224d495f
Sankarshan Dudhate <sandudhate@gmail.com>
2020-09-10 21:53:35 +0530

    manageProducts connected to backend

--- lib/manageProducts.dart
+++ lib/manageProducts.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:collection';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'productCard.dart';
 
 class manageProducts extends StatefulWidget {
   @override
@@ -6,14 +13,85 @@ class manageProducts extends StatefulWidget {
 }
 
 class _manageProductsState extends State<manageProducts> {
-
+  Key listKey = new Key("productListKey");
   String searchName;
   int activeCount = 2;
   int inactiveCount = 3;
   bool productStatus;
+  TextEditingController searchController = new TextEditingController();
+  List activeProdList = new List();
+  List inactiveProdList = new List();
+  bool loadActiveProds = true;
 
-  Widget searchBox(){
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<dynamic>> fetchProducts(bool activeProds) async {
+    log("Active: "+activeProds.toString());
+    String url = 'http://192.168.43.167:5000/products/';
+
+    if(activeProdList.isEmpty && inactiveProdList.isEmpty) {
+      //Only fetch data if it hasn't been fetched before
+      var resp = await http.get(url);
+      var productsList = jsonDecode(resp.body);
+      print(productsList[0]);
+      for(var prod in productsList ) {
+        //print(prod["available"]);
+        if(prod["available"]) {
+          activeProdList.add(prod);
+        } else {
+          inactiveProdList.add(prod);
+        }
+      }
+    }
+
+    if(activeProds) log( activeProdList[0].toString() );
+    else log(inactiveProdList[0].toString());
+    activeCount = activeProdList.length;
+    inactiveCount = inactiveProdList.length;
+    if(activeProds) return activeProdList;
+    else return inactiveProdList;
+  }
+
+  Widget productCardsColumn(bool activeProds) {
+    //print('anfksnkja');
+    return FutureBuilder(
+        future: fetchProducts(activeProds),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          //print(snapshot.data);
+          List prods = snapshot.data.toList();
+          //print(prods);
+          return Column(
+            children: prods.map((prod) => productCard(prod)).toList(),
+          );
+        }
+    );
+  }
+  
+  Widget productCard(cardData) {
+    return product_card(
+      product_id: "#786GFHDR", //need to add product number
+      product_name: cardData["name"],
+      product_price: cardData["price"].toString(),
+      views: cardData["views"].toString(),
+      favorite: cardData["wishlisted"].toString(),
+      enquiries: cardData["enquiries"].toString(),
+      expiry_date: cardData["expiryDate"].toString(),
+      image: "assets/images/product image.png", //change image
+    );
+  }
+
+  void showInactiveProucts() {
+    //return productCardsColumn(false);
+  }
+
+  Widget searchBox() {
     return Container(
+      //height: double.infinity,
       //padding: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(30),),
@@ -26,23 +104,24 @@ class _manageProductsState extends State<manageProducts> {
             ),
           ],
       ),
-      child: TextField(
+      child: TextFormField(
         textAlign: TextAlign.center,
-      decoration: InputDecoration(    
-        fillColor: Colors.grey,
-        hintText: "Search for product name or code",
-        hintStyle: TextStyle(
-        color: Colors.grey[400]
-      ),
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(60),),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              borderSide: BorderSide(color: Colors.grey),
-              //color:Colors.grey,
-            ),
-        suffixIcon: Icon(Icons.search,color: Colors.black87,),
-      ), 
+        decoration: InputDecoration(
+          fillColor: Colors.grey,
+          hintText: "Search for product name or code",
+          hintStyle: TextStyle(
+          color: Colors.grey[400]
+        ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(60),),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                borderSide: BorderSide(color: Colors.grey),
+                //color:Colors.grey,
+              ),
+          suffixIcon: Icon(Icons.search,color: Colors.black87,),
+        ),
+        controller: searchController,
     ),
     );
   }
@@ -50,241 +129,123 @@ class _manageProductsState extends State<manageProducts> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Stack(
-          children:[
-              Container(
-                decoration: BoxDecoration(
-                   color: Colors.grey[300],
-                   borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30.0),
-                      bottomRight: Radius.circular(30.0)),
-                ),
-                  //margin: EdgeInsets.fromLTRB(20,0,50,0),
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 50),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                     IconButton(
-                       icon: Icon(Icons.arrow_back_ios), onPressed: (){
-                        setState(() {
-                          //Navigate
-                        });
-                      }),
-                      //SizedBox(width: 50,),
-                      Column(
-                        children: [
-                          Text("Manage your products", style: TextStyle(fontSize: 18),),
-                        ],
-                      ),
-                      Column(),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(40, 75, 40, 0),
-                  child: searchBox(),
-                )
-          ]
-        ),
-        SizedBox(height: 30,),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal:20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Container(
+        child: ListView(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
             children: [
-              Flexible(
-                child: Container(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black
-                    )
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Active Products",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                    SizedBox(width:5),
-                    Text("(3)",style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
-                  ],
-                ))),
-                SizedBox(width:30),
-              Flexible(child: Container(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black
-                    )
-                  ),
-                ),
-               child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Inactive Products",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                    SizedBox(width:5),
-                    Text("(3)",style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
-                  ],
-                ))),
-            ],
-          ),
-        ),
-        SizedBox(height:30),
-        product_card(
-                        product_id: "#786GFHDR",
-                        product_name: "Cast Iron gears 15 inche 1050 rounded edges - PVC",
-                        product_price: "7000",
-                        views: "112",
-                        favorite: "19",
-                        enquiries: "45",
-                        expiry_date: "12 August 2020",
-                        image: "assets/images/product image.png",
+              Stack(
+                  children:[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30.0),
+                            bottomRight: Radius.circular(30.0)),
                       ),
-        ]),
-    );
-  }
-}
-
-
-
-class product_card extends StatelessWidget {
-  final String product_id;
-  final String product_name;
-  final String product_price;
-  final String expiry_date;
-  final String image;
-  final String views;
-  final String favorite;
-  final String enquiries;
-  const product_card({Key key, this.product_id,this.product_name,this.product_price,this.expiry_date, this.views,this.favorite, this.enquiries,this.image})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 20),
-        padding: EdgeInsets.all(20),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(20),),
-          color: Color(0xffffffff),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 4),
-              blurRadius: 40,
-              color: Colors.black.withOpacity(.16),
-            ),
-          ],
-        ),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset(image, height: 100,),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(product_id,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Color(0xff6e6e6e)),),
-                      SizedBox(height: 3,),
-                      Container(
-                       width: 190,
-                        child: Text(product_name,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black),),
-                      ),
-                      SizedBox(height: 5,),
-                      Row(
+                      //margin: EdgeInsets.fromLTRB(20,0,50,0),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 50),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Image.asset("assets/images/ruppee.png", height: 13,),
-                          Text(" "+product_price,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Color(0xff6F6F6F)),),
+                          IconButton(
+                              icon: Icon(Icons.arrow_back_ios), onPressed: (){
+                            setState(() {
+                              //Navigate
+                            });
+                          }),
+                          //SizedBox(width: 50,),
+                          Column(
+                            children: [
+                              Text("Manage your products", style: TextStyle(fontSize: 18),),
+                            ],
+                          ),
+                          Column(),
                         ],
                       ),
-                      SizedBox(height: 15,),
-                      Container(
-                        height: 15,
-                        child: Row(
-                          children: [
-                            Text("EDIT",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Color(0xff6F6F6F)),),
-                            VerticalDivider(color: Colors.black,),
-                            Text("CHANGE QTY",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Color(0xff6F6F6F)),),
-                          ],
-                        ),
-                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(40, 75, 40, 0),
+                      child: searchBox(),
+                    )
+                  ]
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Flexible(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                // print("dumb-fucking state!");
+                                this.loadActiveProds = true;
+                              });
+                            },
+                            child: Container(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Colors.black
+                                      )
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Active Products",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                    SizedBox(width:5),
+                                    Text("(3)",style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
+                                  ],
+                                )),
+                          )),
+                      SizedBox(width:20),
+                      Flexible(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                // print("dumb-fucking state!");
+                                this.loadActiveProds = false;
+                              });
+                            },
+                            child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      color: Colors.black
+                                  )
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Inactive Products",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                SizedBox(width:5),
+                                Text("(3)",style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
+                              ],
+                            )),
+                          )),
                     ],
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Container(
-              width: double.infinity,
-              height: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10),),
-                color: Color(0xff0B868A),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.remove_red_eye,color: Colors.white,),
-                      Text(views+" views",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.favorite,color: Colors.white,),
-                      Text(favorite+" times",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(enquiries,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: Colors.white),),
-                      Text("Enquiries",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20,),
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween ,
-              children: [
-                Text("Ad expires on "+ expiry_date,
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w600, color: Color(0xff6F6F6F)),),
-                Text("Renew Now",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600, color: Color(0xff4DA4D6)),),
-              ],
-            ),
-
-          ],
+              SizedBox(height:30),
+              productCardsColumn(this.loadActiveProds),
+              // ListView.builder(itemBuilder: (context){
+              //     return
+              // })
+            ]
         ),
+      ),
     );
   }
 }
+
+
+
