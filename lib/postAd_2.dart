bc29c2076c53179a76a1efed4ee0230229b84221
adharashive <adharashive@gmail.com>
2020-09-12 15:56:00 +0530

    UI created for getting and displaying images in post ad and limiting it to 10 images.

--- lib/postAd_2.dart
+++ lib/postAd_2.dart
@@ -1,6 +1,7 @@
 //import 'dart:html';
 
 import 'dart:io';
+import 'dart:math';
 
 import 'package:destock/postAd_3.dart';
 import 'package:flutter/material.dart';
@@ -18,46 +19,72 @@ class postAd_2 extends StatefulWidget {
 class _postAd_2State extends State<postAd_2> {
 
   File _image;
+  final _picker = ImagePicker();
   List<File> allFiles = new List();
   List<File> files = new List();
 
   _fileOpener() async {
     files = await FilePicker.getMultiFile( type: FileType.custom, allowedExtensions: ['jpg','png','jpeg'],);
-    allFiles.addAll(files);
-    print(allFiles);
+    setState(() {
+      this.allFiles.addAll(files);
+    });
+    //print(allFiles);
+    //Navigator.of(context).pop();
   }
 
-  _getImage(BuildContext context) async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-    this.setState(() {
-      _image = image;
-      allFiles.add(_image);
-      print(allFiles);
+  Future _getImage(BuildContext context) async {
+    PickedFile image = await _picker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(image.path);
+      this.allFiles.add(_image);
+      //print(allFiles);
     });
     //Navigator.of(context).pop();
   }
 
-  Widget _showSelectedImages(BuildContext context) {
-    if(allFiles.isEmpty){
-      return _chooseMethod();
-    }
-    else {
-      return _addMore();
-    }
-  }
+  // Widget _showSelectedImages(BuildContext context) {
+  //   if(allFiles.isEmpty){
+  //     return _chooseMethod();
+  //   }
+  //   else {
+  //     return _addMore();
+  //   }
+  // }
 
   Widget _addMore() {
     return Column(
       children: [
-        Text('Images Added!'),
+        _buildImageCard(),
         SizedBox(height: 20,),
         GestureDetector(
           onTap: () {
-              _getImage(context);
+              _buildDialogue(context);
             },
           child: Text('Add More..',style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold))),
-
       ],
+    );
+  }
+
+  void _buildDialogue(BuildContext context) {
+
+    var alertDialog = AlertDialog(
+      content :
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _chooseMethod(),
+            ],
+          ),
+      shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(10.0))
+               ),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      }
     );
   }
 
@@ -66,7 +93,7 @@ class _postAd_2State extends State<postAd_2> {
                 //crossAxisAlignment: CrossAxisAlignment.center,
                 children:[
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: GestureDetector(
                       onTap: () {
                         _getImage(context);
@@ -112,7 +139,7 @@ class _postAd_2State extends State<postAd_2> {
             ),
             SizedBox(height:50),
               Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: GestureDetector(
                       onTap: () {
                         _fileOpener();
@@ -134,7 +161,7 @@ class _postAd_2State extends State<postAd_2> {
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                 Icon(Icons.photo , color: Colors.black,size: 30,),
+                                Icon(Icons.photo , color: Colors.black,size: 30,),
                                 SizedBox(width:30),
                                 Flexible(child: Text("Select From Gallery", style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold))),
                               ],
@@ -144,84 +171,165 @@ class _postAd_2State extends State<postAd_2> {
                   )]);
   }
 
+  _removeFromList(BuildContext context,index){
+    setState(() {
+      this.allFiles.removeAt(index);
+    });
+  }
+
+  Widget _buildImageCard(){
+    return ListView.builder(
+      itemCount: allFiles.length,
+      shrinkWrap: true,
+      itemBuilder: (BuildContext context, index) => Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        margin: EdgeInsets.symmetric(vertical:10),
+        decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5),),
+          color: Color(0xffffffff),
+          // boxShadow: [
+          //   BoxShadow(
+          //     offset: Offset(2, 2),
+          //     blurRadius: 40,
+          //     color: Colors.black.withOpacity(.16),
+          //   ),
+          // ],
+      ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              child: Image.file(allFiles[index],height: 60, width:60),
+            ),
+            //SizedBox(width: 10,),
+            Text("PIC" + (index+1).toString(), style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+                InkWell(
+                  onTap: () {
+                    _removeFromList(context,index);
+                    print(allFiles);
+                  },
+                  child: Icon(Icons.cancel,color: Colors.grey,)
+                  ),
+              ]
+            )
+          ]
+        )
+      )
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+      body: SingleChildScrollView(
+              child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+        children: [
+          SizedBox(height:50),
+          Row(
           children: [
+        Text(
+          "Step 2",
+          style: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 20.0,
+              fontFamily: "Arial",
+              fontWeight: FontWeight.bold),
+        ),
+          ],
+          ),
+          SizedBox(height:10),
+          Row(
+        children: [
+          Text('UPLOAD PRODUCT IMAGES',
+          style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          )),
+        ],
+        ),
+        SizedBox(height: 50),
+        //_showSelectedImages(context),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal:30),
+          child: Column(
+            children: [
+              allFiles.isEmpty ? _chooseMethod()
+              : _addMore(),
+            ],
+          ),
+        ),
+        SizedBox(height: 20,),
             SizedBox(height:50),
             Row(
-            children: [
-              Text(
-                "Step 2",
-                style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 20.0,
-                    fontFamily: "Arial",
-                    fontWeight: FontWeight.bold),
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        Text(
+           "Instructions\n1.Ensure the image is clear\n2. Maximum 10 images can be\nuploaded for a single product",
+          style: TextStyle(
+              color: Colors.grey,
+              fontSize: 14.0,
+              fontFamily: "Arial",
+              //fontWeight: FontWeight.bold
               ),
-            ],
-            ),
-            SizedBox(height:10),
-            Row(
-              children: [
-                Text('UPLOAD PRODUCT IMAGES',
-                style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                )),
-              ],
-              ),
-              SizedBox(height: 50),
-              _showSelectedImages(context),
-                  SizedBox(height:50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                 "Instructions\n1.Ensure the image is clear\n2. Maximum 10 images can be\nuploaded for a single product",
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14.0,
-                    fontFamily: "Arial",
-                    //fontWeight: FontWeight.bold
-                    ),
-              ),
-            ],
-            ),
-            SizedBox(height:30),
-            GestureDetector(
-                    onTap: () {
-                      if(allFiles.isNotEmpty && allFiles.length <10){
-                              //Use allFiles to store into database
-                              Navigator.pushReplacement(context, new MaterialPageRoute(
-                                  builder: (BuildContext context) => new postAd_3() ),
-                              );
-                      }
-                      else{
-                        
-                      }
-                    },
-                        child: Container(
-                            height: 50,
-                            margin: EdgeInsets.symmetric(horizontal: 50),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Colors.redAccent,
-                            ),
-                            child: Center(
-                              child: Text("NEXT", style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.bold)),
-                            ),
-                          ),
-                  ),
-                  SizedBox(height:30)
+        ),
           ],
+          ),
+          SizedBox(height:30),
+          GestureDetector(
+              onTap: () {
+                if(allFiles.isNotEmpty && allFiles.length<=10){
+                        //Use allFiles to store into database
+                        Navigator.pushReplacement(context, new MaterialPageRoute(
+                            builder: (BuildContext context) => new postAd_3() ),
+                        );
+                }
+                else{
+                  _buildErrorDialogue(context);
+                }
+              },
+                  child: Container(
+                      height: 50,
+                      margin: EdgeInsets.symmetric(horizontal: 50),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.redAccent,
+                      ),
+                      child: Center(
+                        child: Text("NEXT", style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+            ),
+            SizedBox(height:30)
+        ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+void _buildErrorDialogue(BuildContext context) {
+
+    var alertDialog = AlertDialog(
+      content : Text("Only 10 Images are Allowed.\n Cancel excess images"),
+      shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.all(Radius.circular(10.0))
+               ),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      }
+    );
+  }
